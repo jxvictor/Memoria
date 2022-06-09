fd84108aa9d321e916f0a5f6b40b5fb088725108
João Victor Costa Alves <57672775+jxvictor@users.noreply.github.com>
2022-06-09 17:43:20 -0300

    Faltando os algoritmos e o restante abaixo

--- src/Gerenciamento/Main.java
+++ src/Gerenciamento/Main.java
@@ -8,7 +8,8 @@ import javax.management.InstanceNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws InstanceNotFoundException {
-    	//int numProcessosRemovidos = Processo.getNumeroAleatorio(1, 2);
+		
+    	int numProcessosRemovidos = Processo.getNumeroAleatorio(1, 2);
         int cont = 0;
         final long time = 1000; // a cada 1 segundo = 1000
 		Timer timer = new Timer();
@@ -18,18 +19,35 @@ public class Main {
 				private int count = 0;
 				double tamTotalProcessosGerados = 0;
 				public void run() {
+					
 					Processo processo;
 					processo = GeradorProcessos.gerar();
-					processo = GeradorProcessos.gerar();
-		        	tamTotalProcessosGerados += processo.getTamanhoAloc();
-		        	System.out.println("PROCESSO CRIADO!");
-		        	System.out.println("Tamanho Alocação: " + processo.getTamanhoAloc());
+					System.out.println("PROCESSO CRIADO!");
 		        	System.out.println("ID: " + processo.getId());
+		        	System.out.println("Tamanho Alocação: " + processo.getTamanhoAloc());
 		        	System.out.println("Posição: " + processo.getPosicao());
 		        	System.out.println("----------------------------------------------------");
+		        	tamTotalProcessosGerados += processo.getTamanhoAloc();
+		        	
+					Processo processo1;
+					processo1 = GeradorProcessos.gerar();
+					System.out.println("PROCESSO CRIADO!");
+		        	System.out.println("ID: " + processo1.getId());
+		        	System.out.println("Tamanho Alocação: " + processo1.getTamanhoAloc());
+		        	System.out.println("Posição: " + processo1.getPosicao());
+		        	System.out.println("----------------------------------------------------");
+		        	
+		        	tamTotalProcessosGerados += processo1.getTamanhoAloc();
+					Memoria memoria = new Memoria();
+					memoria.adicionar(processo);
+					memoria.adicionar(processo1);
+		        	
+		        	Memoria.removerAleatorio();
+		        	System.out.println(Memoria.getProcessos());
+		        	
 		        	count++;
 		        	tamTotalProcessosGerados ++;
-		        	if (count == 100) {
+		        	if (count == 5) {
 		        		timer.cancel();
 		        		System.out.println("-------------RELATÓRIO--------------");
 		                System.out.println("Tamanho médio dos processos gerados: " + tamTotalProcessosGerados/100);
--- src/Gerenciamento/Memoria.java
+++ src/Gerenciamento/Memoria.java
@@ -1,21 +1,41 @@
 package Gerenciamento;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Memoria {
 
     public static final Integer TAMANHO = 1000;
-    private static List<Processo> processos;
+    private static List<Processo> processos = new ArrayList<>();
     
     //private LinkedList<Processo> memoria = new LinkedList<Processo>();
 
-    public void adicionar(Processo processo) {
+    public static List<Processo> getProcessos() {
+		return processos;
+	}
+
+	public static void setProcessos(List<Processo> processos) {
+		Memoria.processos = processos;
+	}
+
+	public void adicionar(Processo processo) {
     	processos.add(processo);
     }
 
     public void remover(Processo processo) {
+    	
     	processos.remove(processo);
+    	
     }
-    
+    public static void removerAleatorio()
+    {
+    	if (processos.size() > 0) {
+        	Random random = new Random();
+        	System.out.println(processos.size());
+        	int id = random.nextInt(processos.size());
+        	processos.remove(id);
+		}
+    }
 
 }
